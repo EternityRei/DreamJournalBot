e7c42e31b4d1fcbe4687a2671a3fa9dd479ae6f8
Nastya Majorova <nmajorova@provectus.com>
2024-04-29 16:45:50 +0300

    Add LLM Agent

--- djbot/djbot/api/commands/start_command.py
+++ djbot/djbot/api/commands/start_command.py
@@ -2,19 +2,26 @@ import os
 
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from djbot.core.agent import Agent
 
 
-async def handle_message(update: Update, context: CallbackContext) -> None:
+async def handle_message(
+        update: Update,
+        context: CallbackContext,
+        agent: Agent
+) -> None:
     user_text = update.message.text
     chat_id = update.message.chat_id
-    response_text = f"You said: {user_text}"
-    await context.bot.send_message(chat_id=chat_id, text=response_text)
+    agent_response = agent.ask(user_text)['output']
+    await context.bot.send_message(chat_id=chat_id, text=agent_response)
 
 
 def main():
     application = Application.builder().token(os.environ.get('BOT_TOKEN')).build()
 
-    text_handler = MessageHandler(filters.TEXT, handle_message)
+    agent = Agent()
+
+    text_handler = MessageHandler(filters.TEXT, lambda update, context: handle_message(update, context, agent))
     application.add_handler(text_handler)
 
     application.run_polling()
---
+++ djbot/djbot/core/tool/search_tool.py
@@ -0,0 +1,6 @@
+from langchain.tools import tool
+
+@tool
+def search():
+    """Used to collect information all over the web based on provided queries"""
+    pass
